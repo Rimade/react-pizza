e8bbde81ad211090b1a048f0046d67b813b5f602
Magomed <ingushotdushi@mail.ru>
2024-05-11 22:01:19 +0300

    update

---
+++ src/components/Categories.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+
+function Categories() {
+	const [activeIndex, setActiveIndex] = useState(0)
+
+	const categories = [
+		'Все',
+		'Мясные',
+		'Вегетарианские',
+		'Гриль',
+		'Острые',
+		'Закрытые',
+	]
+
+	const onClickCategory = (index) => {
+		setActiveIndex(index)
+	}
+
+	return (
+		<div className="categories">
+			<ul>
+				{categories.map((categoryName, index) => (
+					<li
+						key={index}
+						onClick={() => onChangeCategory(index)}
+						className={value === index ? 'active' : ''}
+					>
+						{categoryName}
+					</li>
+				))}
+			</ul>
+		</div>
+	)
+}
+
+export default Categories
